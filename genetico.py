ea88b6ff95043348b748e9843992623b6976884a
paulorcmendes <mendesdpn@gmail.com>
2018-06-09 11:23:25 -0300

    Função de Fitness feita

--- genetico.py
+++ genetico.py
@@ -6,35 +6,38 @@ Created on Fri Jun 08 23:40:34 2018
 """
 import numpy as np
 
-A = [4,3,2,3]
-lenA = len(A)
-V = 5
+def BinToInt(nBin):
+    return int(''.join(str(x) for x in nBin), 2)
 
-def fitness(chrm):
+def fitness(chrm, A, sizePerEntry):       
+    fit = 1    
+    MChrm = np.reshape(chrm, (len(A), sizePerEntry))
+    
+    X = np.full((len(A),len(A)),0)
+    for i in range(len(A)):
+        X[i][int(np.around((len(A)-1)*float(BinToInt(MChrm[i]))/((2**sizePerEntry) - 1)))] = 1
        
-    fit = 0
-    aux = np.reshape(chrm, (lenA, lenA))
-    M1 = np.full((lenA, 1),1)    
-    print aux    
-    if np.matmul(aux,M1) == M1 and np.matmul(A,aux) <= V:
-        fit+=lenA*10 #só colocando pra ja ficar bem melhor que as que violam
-        for i in range lenA:
-            for j in range lenA:
-                if aux[j][i] == 1:
+    M1 = np.full((len(A), 1),1)   
+     
+    if (np.matmul(X,M1) == M1).all() and (np.matmul(A,X) <= V).all():
+        #A primeira restrição nunca é violada, btw
+        fit+=len(A)*3 #só colocando pra ja ficar bem melhor que as que violam
+        for i in range(len(A)):
+            has1 = False
+            for j in range(len(A)):
+                if X[j][i] == 1:
+                    has1 = True
                     break
-            if j == lenA:
-                fit+=1
+            if has1 == False:
+                fit+=2
     return fit
         
+A = [4,3,2,4]
+lenA = len(A)
+V = 7
 
 
+sizePerEntry = int(np.log2(lenA-1))*5
+chrm = np.random.randint(0, 2, lenA*sizePerEntry)
 
-
-chrm = np.random.randint(0, 2, lenA**2)
-
-
-
-
-    
-    
-        
+print fitness(chrm, A, sizePerEntry)    
